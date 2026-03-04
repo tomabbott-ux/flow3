42c865b94ad481141c41a87d6eb29efb94570da6
Tom Abbott <tom_abbott@mac.com>
2026-03-04 09:55:57 +0000

    Initial Commit

---
+++ Flow3/Flow3App.swift
@@ -0,0 +1,17 @@
+//
+//  Flow3App.swift
+//  Flow3
+//
+//  Created by Tom Abbott on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Flow3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
