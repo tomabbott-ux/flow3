3fff231fac128aac30385cd77b2694f4198ec254
Tom <Tom>
2026-03-10 23:19:03 +0000

    Weather Pill Accurate 10th March 23.20

---
+++ Flow3/Weather/AviationWeatherMETARProvider.swift
@@ -0,0 +1,8 @@
+//
+//  AviationWeatherMETARProvider.swift
+//  Flow3
+//
+//  Created by Tom Abbott on 10/03/2026.
+//
+
+import Foundation
---
+++ Flow3/Weather/FlowAirport+ICAO.swift
@@ -0,0 +1,8 @@
+//
+//  FlowAirport+ICAO.swift
+//  Flow3
+//
+//  Created by Tom Abbott on 10/03/2026.
+//
+
+import Foundation
